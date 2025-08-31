36abb7d42518cabae15a3b20c3bd90cf9b8ecf33
marwaMRadwan <marwaMRadwan>
2025-08-31 14:25:54 +0300

    s11

---
+++ session11/js/custom.js
@@ -0,0 +1,48 @@
+// console.log("hello from js file")
+
+// single comment
+
+/*
+hhhh
+jhj
+nkn
+bjb
+*/
+
+// variables
+// any (number, string, boolearn, object, .....)
+
+// var let const
+// var x = 5
+// var x = "marwa"
+// var x = false
+// var x = "ay 7aga"
+
+// let x = 5
+// x =false
+
+// const a = 5
+
+// a=7
+
+// arithmatic operation
+// + - * / () ** %
+// let x = 5 + 3
+// assignment
+// = += -= /= .....
+// let z = 3
+// z += 2
+
+//  copmarison 
+// == === >= <= != !== > <....
+// let a = 5, b = "5"
+// console.log(typeof a, typeof b)
+// console.log(a === b)
+
+// ternery 
+// (cond)? if true : if false
+// a === b ? console.log("a and b identical") : console.log("not identical")
+
+
+// logical operators 
+// && || !
